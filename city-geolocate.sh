eb10b66187f83ea6c4a870d8a6259892b9e8c98b
Zack Matthews <zackary.matthews@yahoo.com>
2024-06-16 14:20:24 -0700

    Fix: Added debugging echo statements

--- city-geolocate.sh
+++ city-geolocate.sh
@@ -8,9 +8,19 @@ source ./.env
 OUTPUT_FILE=cityId.txt
 
 PUBLIC_IP=`curl -s https://ipinfo.io/ip`
+echo "Public IP: ${PUBLIC_IP}"
 
 # Call the geolocation API and capture the output and save the JSON output
 RESPONSE=`curl -s http://api.ipstack.com/${PUBLIC_IP}?access_key=${IPSTACK_API_KEY} | jq 'del(.location.is_eu)'`
+echo "Geolocation API Response: ${RESPONSE}"
+
+PROCESSED_RESPONSE=$(echo ${RESPONSE} | jq 'walk(if type == "object" then with_entries(select(.value != null)) else . end)')
+echo "Processed Response: ${PROCESSED_RESPONSE}"
+
 
 echo -n "" > ~/.config/qtile/current_location.py
-echo data=${RESPONSE} >> ~/.config/qtile/current_location.py
+
+echo data=${PROCESSED_RESPONSE} >> ~/.config/qtile/current_location.py
+echo -n "" > ${OUTPUT_FILE}
+
+cat ${OUTPUT_FILE}
